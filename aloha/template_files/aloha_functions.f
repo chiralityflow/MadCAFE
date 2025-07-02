34b87b2244c09119cc1d37db25dac504ba7c402a
Zenny Wettersten <zenny.wettersten@cern.ch>
2025-07-02 14:14:33 +0200

    finally proper normalisation of the q vectors

--- aloha/template_files/aloha_functions.f
+++ aloha/template_files/aloha_functions.f
@@ -234,7 +234,7 @@ c
       double precision pfl(0:3), pflRot(0:3), qLoc(0:3)
       integer nhel,nsv,nsvahl,nsvhel, i
 
-      double precision rZero, rHalf, rOne, rTwo, rHalfSqH,rPosPrefac, rNegPrefac, threeMomProd, pfront, pback
+      double precision rZero, rHalf, rOne, rTwo, rHalfSqH,rPosPrefac, rNegPrefac, threeMomProd, pfront, pback, invMomProd
       parameter( rZero = 0.0d0, rHalf = 0.5d0 )
       parameter( rOne = 1.0d0, rTwo = 2.0d0 )
 
@@ -255,10 +255,11 @@ c
       ! qLoc(0:3) = q(0:3)
 c ZW: hard-coding helicity basis for now
       threeMomProd = dsqrt(p(1)**2 + p(2)**2 + p(3)**2)
+      invMomProd = 1.0d0 / threeMomProd
       pfront = p(0) + threeMomProd
       pback = p(0) - threeMomProd
       qLoc(0) = 0.5d0 * pback
-      qLoc(1:3) = -0.5d0*p(1:3)
+      qLoc(1:3) = -0.5d0*pback**invMomProd*p(1:3)
 
       alpha = rHalf*(vmass**2)/(p(0)*qLoc(0) - p(1)*qLoc(1) - p(2)*qLoc(2) - p(3)*qLoc(3))
 
@@ -366,7 +367,7 @@ c
       integer nhel,nsv,nsvahl,nsvhel,i
       double precision invMass
 
-      double precision rZero, rHalf, rOne, rTwo, rHalfSqH,rPosPrefac, rNegPrefac, threeMomProd
+      double precision rZero, rHalf, rOne, rTwo, rHalfSqH,rPosPrefac, rNegPrefac, threeMomProd, pfront, pback, invMomProd
       parameter( rZero = 0.0d0, rHalf = 0.5d0 )
       parameter( rOne = 1.0d0, rTwo = 2.0d0 )
 
@@ -386,8 +387,11 @@ c
       ! qLoc(0:3) = q(0:3)
 c ZW: hard-coding helcity basis for now
       threeMomProd = dsqrt(p(1)**2 + p(2)**2 + p(3)**2)
-      qLoc(0) = 0.5d0 * (p(0) - threeMomProd)
-      qLoc(1:3) = -0.5d0*p(1:3)
+      invMomProd = 1.0d0 / threeMomProd
+      pfront = p(0) + threeMomProd
+      pback = p(0) - threeMomProd
+      qLoc(0) = 0.5d0 * pback
+      qLoc(1:3) = -0.5d0*pback*invMomProd*p(1:3)
 
       alpha = rHalf*(vmass**2)/(p(0)*qLoc(0) - p(1)*qLoc(1) - p(2)*qLoc(2) - p(3)*qLoc(3))
 
@@ -497,7 +501,7 @@ c
       double precision p(0:3),vmass,invMass,hel,hel0,pzpt,emp,sqh,q(0:3),sqp0p3,sqq0q3, alpha, pfl(0:3), pflRot(0:3), qLoc(0:3)
       integer nhel,nsv,nsvahl,nsvhel,i
 
-      double precision rZero, rHalf, rOne, rTwo, rHalfSqH,rPosPrefac, rNegPrefac, threeMomProd
+      double precision rZero, rHalf, rOne, rTwo, rHalfSqH,rPosPrefac, rNegPrefac, threeMomProd, pfront, pback, invMomProd
       parameter( rZero = 0.0d0, rHalf = 0.5d0 )
       parameter( rOne = 1.0d0, rTwo = 2.0d0 )
 
@@ -529,8 +533,11 @@ c
       ! qLoc(0:3) = q(0:3)
 c ZW: hard-coding helcity basis for now
       threeMomProd = dsqrt(p(1)**2 + p(2)**2 + p(3)**2)
-      qLoc(0) = 0.5d0 * (p(0) - threeMomProd)
-      qLoc(1:3) = -0.5d0*p(1:3)
+      invMomProd = 1.0d0 / threeMomProd
+      pfront = p(0) + threeMomProd
+      pback = p(0) - threeMomProd
+      qLoc(0) = 0.5d0 * pback
+      qLoc(1:3) = -0.5d0*invMomProd*pback*p(1:3)
 
       alpha = rHalf*(vmass**2)/(p(0)*qLoc(0) - p(1)*qLoc(1) - p(2)*qLoc(2) - p(3)*qLoc(3))
 
